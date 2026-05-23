a58ffd5bce418218bd86c622c37132203dededc5
ganesh-karanam <ganeshprofilegk@gmail.com>
2026-05-23 20:36:50 +0530

    Added the models Category Image Product

---
+++ src/main/java/com/ganeshworks/com/ecommerce/model/Product.java
@@ -0,0 +1,29 @@
+package com.ganeshworks.com.ecommerce.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Product {
+    @Id
+    private Long id;
+    private String name;
+    private String description;
+    private BigDecimal price;
+    private int inventory;
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "category_id")
+    private Category category;
+    @OneToMany(mappedBy = "product", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Image> Images;
+}
